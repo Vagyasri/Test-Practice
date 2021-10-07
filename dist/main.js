4b45a8f46b0a15c089f4acaa8e58b83409f5fb0f
Vagyasri <vagyasri2011@gmail.com>
2021-10-07 11:09:50 +0200

    Add tests for the new functionality

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ ((module) => {
 
-eval("// const stringLength = (string) => {\n// return string.length;\n// }\n\nconst stringLength = (string) => {\n    let charCount = 0;\n    for (let i = 1; i <= string.length; i++) {\n        charCount = i;\n    }\n    if (charCount > 10) {\n        throw 'Error';\n    }\n    return charCount;\n}\n\nconsole.log(stringLength('Luckyllllll'));\n\nmodule.exports = stringLength;\n\n//# sourceURL=webpack://webpack-setup/./src/index.js?");
+eval("// const stringLength = (string) => {\n// return string.length;\n// }\n\nconst stringLength = (string) => {\n    let charCount = 0;\n    for (let i = 0; i <= string.length; i++) {\n        charCount = i;\n    }\n    try {\n        if (charCount < 1) {\n            throw 'Error';\n        }\n        if (charCount > 10) {\n            throw 'Condition does not meet';\n        }\n    }\n    finally {\n    }\n    return charCount;\n}\n\nconsole.log(stringLength('lucky'));\n\nmodule.exports = stringLength;\n\n//# sourceURL=webpack://webpack-setup/./src/index.js?");
 
 /***/ })
 
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,7 @@
 const stringLength = require('./index');
 
-test('String length of Lucky:', () => {
+test('String length of:', () => {
     expect(stringLength('Lucky')).toBe(5);
+    expect(() => stringLength('')).toThrowError('Error');
+    expect(() => stringLength('Bhagyashree')).toThrow('Condition does not meet');
 });
