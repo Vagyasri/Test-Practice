83e4326f064be1607b54237af0aaf7af43652646
Vagyasri <vagyasri2011@gmail.com>
2021-10-09 15:27:31 +0200

    Update

--- src/index.test.js
+++ src/index.test.js
@@ -13,22 +13,22 @@ test('Reverse of:', () => {
 describe('A simple calculator for operating on two parameters', () => {
   test('Adds 2 numbers', () => {
     expect(newCalculator.add(7, 9)).toEqual(16);
-    expect(newCalculator.add(7, 9)).toEqual(16);
-    expect(newCalculator.add(7, 9)).toEqual(16);
+    expect(newCalculator.add(8, 5)).toEqual(13);
+    expect(newCalculator.add(8, 7, 8)).toEqual(15);
   });
-  test('Adds 2 numbers', () => {
+  test('Subtracts 2 numbers', () => {
     expect(newCalculator.subtract(9, 7)).toEqual(2);
-    expect(newCalculator.subtract(9, 7)).toEqual(2);
-    expect(newCalculator.subtract(9, 7)).toEqual(2);
+    expect(newCalculator.subtract(8, 5)).toEqual(3);
+    expect(newCalculator.subtract(8, 7, 8)).toEqual(1);
   });
-  test('Adds 2 numbers', () => {
+  test('Divides 2 numbers', () => {
     expect(newCalculator.divide(9, 3)).toEqual(3);
-    expect(newCalculator.divide(9, 3)).toEqual(3);
-    expect(newCalculator.divide(9, 3)).toEqual(3);
+    expect(newCalculator.divide(7, 3.5)).toEqual(2);
+    expect(newCalculator.divide(5, 2)).toEqual(2.5);
   });
-  test('Adds 2 numbers', () => {
+  test('Multiplies 2 numbers', () => {
     expect(newCalculator.multiply(7, 9)).toEqual(63);
-    expect(newCalculator.multiply(7, 9)).toEqual(63);
-    expect(newCalculator.multiply(7, 9)).toEqual(63);
+    expect(newCalculator.multiply(15, 9)).toEqual(135);
+    expect(newCalculator.multiply(14, 2)).toEqual(28);
   });
 });
